9055d922d32bf91f1d2cf3ec52a9647e11ee462e
Nate Sales <nate@nate.to>
2019-12-13 19:50:05 -0800

    Replace user.uid to firebase's one

--- public/app.js
+++ public/app.js
@@ -1,6 +1,5 @@
 // Sentry.init({ dsn: 'https://9bf86cf07c6f4411ae05cf83a031c332@sentry.io/1848400' });
 
-let user; // Authenticated user object
 let db; // Firebase cloud firestore
 let storage; // Firebase GCP Bucket
 
@@ -25,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /**
  * Does doc exist?
- * @param {object} docRef Firestore document. For example db.collection("supervisors").doc(user.uid)
+ * @param {object} docRef Firestore document. For example db.collection("supervisors").doc(firebase.auth().currentUser.uid)
  */
 function _exists(docRef) {
     docRef.get().then(function (doc) {
@@ -40,9 +39,7 @@ function supervisorSignIn() {
     const provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(provider).then(result => {
-        user = result.user;
-
-        let supervisor = db.collection("supervisors").doc(user.uid);
+        let supervisor = db.collection("supervisors").doc(firebase.auth().currentUser.uid);
 
         if (!_exists(supervisor)) { // If supervisor doesn't exist
             console.log("supervisorSignIn(): Supervisor doesn't exist.");
@@ -69,10 +66,10 @@ function supervisorSignIn() {
 function createUser(name) {
     return db.collection("users").add({ // Create the user
         name: name,
-        supervisors: [user.uid], // With the current supervisor pre-authorized.
+        supervisors: [firebase.auth().currentUser.uid], // With the current supervisor pre-authorized.
         projects: []
     }).then(function (docRef) {
-        db.collection("supervisors").doc(user.uid).update({
+        db.collection("supervisors").doc(firebase.auth().currentUser.uid).update({
             users: firebase.firestore.FieldValue.arrayUnion(docRef.id)
         });
     });
@@ -110,7 +107,7 @@ function removeSupervisor(supervisor, user) {
  * @returns {array} Users //TODO
  */
 function getUsers() {
-    db.collection("supervisors").doc(user.uid).get().then(function (doc) {
+    db.collection("supervisors").doc(firebase.auth().currentUser.uid).get().then(function (doc) {
         users = doc.data()["users"];
 
         for (i = 0; i < users.length; i++) {
@@ -134,7 +131,7 @@ function createProject(name, desc, image_url) {
         name: name,
         desc: desc,
         image_url: image_url,
-        authorized_users: [user.uid], // Pre-authorize current supervisor for use in this project.
+        authorized_users: [firebase.auth().currentUser.uid], // Pre-authorize current supervisor for use in this project.
         tasks: []
     }).then(function (docRef) {
         console.log("Created project " + docRef.id);
