2e6d4379b9cef17b767c77d4336b3bd8972bd704
Nate Sales <nate@nate.to>
2020-03-05 09:51:17 -0800

    New database schema and auth

--- database.py
+++ database.py
@@ -10,7 +10,21 @@ import gridfs
 import pymongo
 from bson.objectid import ObjectId
 
+import bcrypt
+
 import validators as valid
+
+
+class State:
+    PENDING = 1
+    PROBLEM = 2
+    COMPLETE = 3
+
+
+class Role:
+    SUPERVISOR = 1
+    USER = 2
+    ADMIN = 3
 
 
 class JonAppDatabase:
@@ -21,10 +35,11 @@ class JonAppDatabase:
         self._gridfs = gridfs.GridFS(self._db)
 
         # Collections
-        self._supervisors = self._db["supervisors"]
-        self._projects = self._db["projects"]
-        self._tasks = self._db["tasks"]
-        self._users = self._db["users"]
+        self.supervisors = self._db["supervisors"]
+        self.projects = self._db["projects"]
+        self.users = self._db["users"]
+
+        self.salt = "$2b$12$MfET9FATeW4fyP3f3OInGe"
 
     def get_image(self, object_id):  # TODO: No content_type checking. This should only allow images
         entry = self._gridfs.get(object_id)
@@ -36,47 +51,56 @@ class JonAppDatabase:
         content = entry.read()
         return "data:" + content_type + ";base64, " + base64.b64encode(content).decode()
 
+    def put_image(self, image):
+        if image:
+            return self._gridfs.put(image, content_type=image.content_type, filename=image.filename)
+        else:
+            return ""
+
+    def delete_image(self, string_id):
+        return self._gridfs.delete(ObjectId(string_id))
+
     def add_supervisor(self, name, email):
         if not valid.email(email):
             raise ValueError("Invalid email in add_supervisor")
 
-        return str(self._supervisors.insert_one({
+        return str(self.supervisors.insert_one({
             "name": name,
             "email": email
         }).inserted_id)
 
     def add_user(self, name, supervisor):
-        return str(self._users.insert_one({
+        return str(self.users.insert_one({
             "name": name,
             "supervisors": [supervisor],
             "projects": []
         }).inserted_id)
 
-    def add_project(self, name, description):
-        return str(self._projects.insert_one({
+    def add_project(self, name, description, image):
+        return str(self.projects.insert_one({
             "name": name,
             "description": description,
+            "image": self.put_image(image),
+            "users": [],
             "tasks": []
         }).inserted_id)
 
     def add_task(self, project, name, description, image):
-        task_id = str(self._tasks.insert_one({
-            "project": project,
-            "name": name,
-            "description": description,
-            "image": self._gridfs.put(image, content_type=image.content_type, filename=image.filename)
-        }).inserted_id)
-
-        if not task_id:
+        if not self.projects.find_one({"_id": ObjectId(project)}):
             raise ValueError("Project id: " + project + " not found.")
 
-        self._projects.update_one({"_id": ObjectId(project)}, {"$push": {"tasks": task_id}})
-        return task_id
+        self.projects.update_one({"_id": ObjectId(project)}, {"$push": {"tasks": {
+            "name": name,
+            "description": description,
+            "image": self.put_image(image),
+            "state": State.PENDING,
+            "subtasks": []
+        }}})
 
     def get_tasks_html(self, project):
         tasks_html = ""
 
-        tasks = self._projects.find_one({"_id": ObjectId(project)})["tasks"]
+        tasks = self.projects.find_one({"_id": ObjectId(project)})["tasks"]
         for task in tasks:
             task = self._tasks.find_one({"_id": ObjectId(task)})
 
@@ -114,6 +138,27 @@ class JonAppDatabase:
 
         return tasks_html
 
-# def delete_task(self, string_id):
-#     image_id = self._users.find_one({'_id': ObjectId(string_id)})["tasks"]
-# # return self._gridfs.delete(ObjectId(string_id))
+    def register(self, email, password, role=Role.SUPERVISOR):
+        self.users.insert_one({
+            "email": email,
+            "hash": bcrypt.hashpw(password.encode(), self.salt.encode()),
+            "role": role
+        })
+
+    def login(self, email, password):
+        userdoc = self.users.find_one({"email": email})
+
+        if userdoc:
+            return bcrypt.checkpw(password.encode(), userdoc["hash"])
+        else:
+            return False
+
+
+database = JonAppDatabase("mongodb://jonapp.nate.to:5000/")
+
+# database.register("nate@nate.to", "mypassword")
+
+# print(database.login("nate@nate.to", "mypass1word"))
+
+# database.add_project("My project", "project dscr", "")
+# database.add_task("5e6130ca53074c9fe1e5e6d6", "Project Name", "Project descr", "")
