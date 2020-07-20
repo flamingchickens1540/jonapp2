ee935646b7b6ea07dc0da22201f5130f17af7e3f
Nate Sales <nate@nate.to>
2020-07-19 23:55:43 -0700

    Add database method docstrings

--- database.py
+++ database.py
@@ -29,7 +29,14 @@ class JonAppDatabase:
         self.projects = self.db["projects"]
         self.users = self.db["users"]
 
+    # Image methods
+
     def get_image(self, object_id: str):
+        """
+        Get an image from GridFS and convert to base64 string
+        :param object_id: Image ID
+        :return: base64 string
+        """
         try:
             entry = self.gridfs.get(object_id)
         except gridfs.errors.NoFile:
@@ -42,6 +49,11 @@ class JonAppDatabase:
         return "data:" + entry.contentType + ";base64, " + base64.b64encode(entry.read()).decode()
 
     def put_image(self, image):
+        """
+        Upload an image to GridFS
+        :param image: Image
+        :return: Error as string
+        """
         if image and image.filename:
             try:
                 extension = image.filename.split(".")[1]
@@ -49,16 +61,28 @@ class JonAppDatabase:
                 return "No image extension specified"
             else:
                 if image.content_type.split("/")[0] == "image":
-                    return self.gridfs.put(image, content_type=image.content_type, filename="image-" + random_string() + "." + extension)
+                    self.gridfs.put(image, content_type=image.content_type, filename="image-" + random_string() + "." + extension)
         else:
             return "No image specified"
 
     def delete_image(self, string_id):
+        """
+        Delete a GridFS image
+        :param string_id: Image ID
+        :return:
+        """
         return self.gridfs.delete(bson.objectid.ObjectId(string_id))
 
     # Projects
 
     def create_project(self, name: str, description: str, image, user: str):
+        """
+        Create a new project
+        :param name: Project name
+        :param description: Project description
+        :param image: Project image
+        :param user: First user to authorize to access this project
+        """
         # TODO: Type checking and error handling
 
         new_project = self.projects.insert_one({
@@ -71,31 +95,47 @@ class JonAppDatabase:
         self.users.update_one({"_id": user}, {"$push": {"projects": str(new_project.inserted_id)}})
 
     def update_project(self, project_id: str, name: str, description: str, image):
+        """
+        Update a project
+        :param project_id: Project's ID
+        :param name: New project name
+        :param description: New project description
+        :param image: New project image
+        """
         self.projects.update_one({'_id': bson.objectid.ObjectId(project_id)}, {'$set': {
             "name": name,
             "description": description,
             "image": self.put_image(image),
         }})
 
-    def delete_project(self, project_id: str, user: str):
+    def delete_project(self, project_id: str):
+        """
+        Delete a project
+        :param project_id: Project to delete
+        """
         project_doc = self.projects.find_one({"_id": bson.objectid.ObjectId(project_id)})
-
         if not project_doc:  # If project with project_id doesn't exist, no need to do anything.
             return
         else:  # If it does exit, delete it.
-            if user in project_doc["users"]:
-                project_image = project_doc["image"]
+            project_image = project_doc["image"]
+            if project_image:
+                self.delete_image(project_image)
 
-                if project_image:
-                    self.delete_image(project_image)
-
-                self.projects.delete_one({"_id": bson.objectid.ObjectId(project_id)})
+            self.projects.delete_one({"_id": bson.objectid.ObjectId(project_id)})
 
     # Tasks
 
-    def add_task(self, project_id: str, name: str, description: str, image):
+    def create_task(self, project_id: str, name: str, description: str, image):
+        """
+        Create a task
+        :param project_id: Project ID to create task under
+        :param name: Task's name
+        :param description: Task's description
+        :param image: Task's image
+        :return: Error as string
+        """
         if not self.projects.find_one({"_id": bson.objectid.ObjectId(project_id)}):
-            return "Project not found"  # TODO: Real error page
+            return "Project not found"
 
         self.projects.update_one({"_id": bson.objectid.ObjectId(project_id)}, {"$push": {"tasks": {
             "name": name,
@@ -105,9 +145,18 @@ class JonAppDatabase:
             "subtasks": []
         }}})
 
-    def update_task(self, project_id: str, task: str, name: str, description: str, image):
+    def update_task(self, project_id: str, task: str, name: str, description: str, image) -> str:
+        """
+        Update a task
+        :param project_id: ID of project
+        :param task: Task integer
+        :param name: New task name
+        :param description: New task description
+        :param image: New task image
+        :return: Error as string
+        """
         if not self.projects.find_one({"_id": bson.objectid.ObjectId(project_id)}):
-            return "Project not found"  # TODO: Real error page
+            return "Project not found"
 
         if image == 'none':
             self.projects.update_one({"_id": bson.objectid.ObjectId(project_id)}, {"$set": {"tasks." + task: {  # Append the new task
@@ -122,6 +171,11 @@ class JonAppDatabase:
             }}})
 
     def delete_task(self, project_id, task_id):
+        """
+        Delete a task
+        :param project_id: Project that task belongs to
+        :param task_id: Task integer
+        """
         print("Deleting index " + str(task_id) + " from " + project_id)
 
         project_list = self.projects.find_one({"_id": bson.objectid.ObjectId(project_id)})["tasks"]
@@ -132,6 +186,14 @@ class JonAppDatabase:
     # Authentication
 
     def signup(self, email: str, name: str, password: str, user_type: str) -> bool:
+        """
+        Register a new user
+        :param email: User's email
+        :param name: User's name
+        :param password: User's plaintext password
+        :param user_type: Type of user (user || supervisor)
+        :return: True if user's email already exists in database, False otherwise
+        """
         if self.users.find_one({"email": email}):  # If account already exists
             return True  # Account already exists
 
@@ -145,6 +207,12 @@ class JonAppDatabase:
         return False  # Account doesn't already exist
 
     def login(self, email: str, password: str) -> str:
+        """
+        Generate an authentication token
+        :param email: User's email
+        :param password: User's password
+        :return: Authentication token
+        """
         user_doc = self.users.find_one({"email": email})
 
         if user_doc and bcrypt.checkpw(password.encode(), user_doc["password"]):
@@ -155,44 +223,58 @@ class JonAppDatabase:
             return ""
 
     def user_by_token(self, raw_token) -> any:  # TODO: What type of object is this?
+        """
+        Parse a raw token cookie and return user object
+        :param raw_token: Authentication token
+        :return: User document
+        """
         if (raw_token is not None) and (len(raw_token.split("*")) == 2):
             user_id = raw_token.split("*")[0]
-
+            token = raw_token.split("*")[1]
+            
             try:
-                garbage = bson.objectid.ObjectId(user_id)
+                user_object = self.users.find_one({"_id": bson.objectid.ObjectId(user_id)})
             except bson.errors.InvalidId:  # Reject bad ObjectIds
                 return None
-
-            token = raw_token.split("*")[1]
-            user_object = self.users.find_one({"_id": bson.objectid.ObjectId(user_id)})
 
             if token in user_object.get("tokens"):
                 return user_object
         else:
             return None
 
-    def is_authorized(self, token, target_id) -> bool:
-        user_id, token, user_object = self.parse_token(token)
-
-        if user_object and (token.split("*")[1] in user_object["tokens"]):
-            target_object = self.projects.find_one({"_id": bson.objectid.ObjectId(target_id)})
-            return target_object and (user_id in target_object["users"])
-
-        return False
+    # # Deprecate this
+    # def is_authorized(self, token, target_id) -> bool:
+    #     user_id, token, user_object = self.parse_token(token)
+    #
+    #     if user_object and (token.split("*")[1] in user_object["tokens"]):
+    #         target_object = self.projects.find_one({"_id": bson.objectid.ObjectId(target_id)})
+    #         return target_object and (user_id in target_object["users"])
+    #
+    #     return False
 
     # Getters
 
     def get_project(self, project_id: str) -> any:
+        """
+        Get a single project
+        :param project_id: ID of project document
+        :return: Project document
+        """
         try:
             return self.projects.find_one({"_id", bson.objectid.ObjectId(project_id)})
         except bson.errors.InvalidId:
             return None
 
     def get_projects(self, user_doc: any) -> list:
+        """
+        Get all projects for a user
+        :param user_doc: User document
+        :return: List of projects
+        """
         projects = []
 
         for project_id in user_doc.get("projects"):
-            project = self.projects.find_one({"_id": project_id})
+            project = self.get_project(project_id)
             if project:
                 projects.append(project)
 
